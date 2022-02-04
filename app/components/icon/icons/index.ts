9bf5002d3837a09b8984f42787fb477c1dda891a
U170709005 <almiragurkan@posta.mu.edu.tr>
2022-02-04 17:29:34 +0300

    line styles fixed

--- app/components/icon/icons/index.ts
+++ app/components/icon/icons/index.ts
@@ -5,7 +5,7 @@ export const icons = {
   iconsPlayButton: require("./icons-play-button2.png"),
   iconsPauseButton: require("./icons-pause-button1.png"),
   iconsStopCircleButton: require("./icons-stop-circled.png"),
-  iconsStopButton: require("./icons-stop.png"),
+  iconsStopButton: require("./icons-stop2.png"),
   iconsSettingsButton: require("./icons-settings.png"),
   iconsDropdownButton: require("./dropdown.png"),
   iconsDropdownRightButton: require("./dropdown-right.png"),
